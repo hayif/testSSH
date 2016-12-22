3f7ddc50f2c07f7d858fe78709c75b9ac6a0957f
sa <Ppa3pg.126.com>
2016-12-22 15:32:55 +0800

    demo3

---
+++ src/org/demo/Test.java
@@ -0,0 +1,10 @@
+package org.demo;
+
+/**
+ * Created by Administrator on 2016/12/22.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
